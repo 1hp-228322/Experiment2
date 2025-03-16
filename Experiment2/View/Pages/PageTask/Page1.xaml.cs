664cf9017381fdeff366e55598059d568496fc32
1hp <keshasuper396@gmail.com>
2025-03-16 20:49:04 +0300

    Fix

--- Experiment2/View/Pages/PageTask/Page1.xaml.cs
+++ Experiment2/View/Pages/PageTask/Page1.xaml.cs
@@ -24,54 +24,6 @@ namespace Experiment2.View.Pages.PageTask
         {
             InitializeComponent();
         }
-
-    }
-
-    public class Address
-    {
-        private int _index;
-        private string? _country;
-        private string? _city;
-        private string? _street;
-        private string? _house;
-        private string? _apartment;
-
-        public int Index
-        {
-            get => _index;
-            set => _index = value;
-        }
-
-        public string? Country
-        {
-            get => _country;
-            set => _country = value;
-        }
-
-        public string? City
-        {
-            get => _city;
-            set => _city = value;
-        }
-
-        public string? Street
-        {
-            get => _street;
-            set => _street = value;
-        }
-
-        public string? House
-        {
-            get => _house;
-            set => _house = value;
-        }
-
-        public string? Apartment
-        {
-            get => _apartment;
-            set => _apartment = value;
-        }
-
         private void BtnGetData_Click(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(TbIndex.Text) ||
@@ -86,5 +38,100 @@ namespace Experiment2.View.Pages.PageTask
                                 MessageBoxButton.OK,
                                 MessageBoxImage.Error);
             }
+            else
+            {
+                Address address = new()
+                {
+                    Index = Convert.ToInt32(TbIndex.Text),
+                    Country = TbCountry.Text,
+                    City = TbCity.Text,
+                    Street = TbStreet.Text,
+                    House = TbHouse.Text,
+                    Apartment = TbApartment.Text
+                };
 
+                MessageBox.Show($"Почтовый индекс - {address.Index}\n" +
+                                $"Страна - {address.Country}\n" +
+                                $"Город - {address.City}\n" +
+                                $"Улица - {address.Street}\n" +
+                                $"Дом - {address.House}\n" +
+                                $"Квартира - {address.Apartment}",
+                                "Задание 1",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Information);
+
+                ClearFields();
+            }
+        }
+
+        private void ClearFields()
+        {
+            TbIndex.Text = string.Empty;
+            TbCountry.Text = string.Empty;
+            TbCity.Text = string.Empty;
+            TbStreet.Text = string.Empty;
+            TbHouse.Text = string.Empty;
+            TbApartment.Text = string.Empty;
+        }
+
+        private void TbStreet_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Tbi_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+
+    public class Address
+    {
+            private int _index;
+            private string? _country;
+            private string? _city;
+            private string? _street;
+            private string? _house;
+            private string? _apartment;
+
+            public int Index
+            {
+                get => _index;
+                set => _index = value;
+            }
+
+            public string? Country
+            {
+                get => _country;
+                set => _country = value;
+            }
+
+            public string? City
+            {
+                get => _city;
+                set => _city = value;
+            }
+
+            public string? Street
+            {
+                get => _street;
+                set => _street = value;
+            }
+
+            public string? House
+            {
+                get => _house;
+                set => _house = value;
+            }
+
+            public string? Apartment
+            {
+                get => _apartment;
+                set => _apartment = value;
+            }
+
+
+    }
+}
+    
 
