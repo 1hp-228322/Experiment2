b18b1f840b58798c55b077da72ec6cb2fbcd768d
1hp <keshasuper396@gmail.com>
2025-03-18 20:43:06 +0300

    Page3

--- Experiment2/View/Pages/PageTask/Page3.xaml.cs
+++ Experiment2/View/Pages/PageTask/Page3.xaml.cs
@@ -13,13 +13,46 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Experiment2.View.Pages.PageTask
+
+namespace CourseWorkApp.View.Pages.PageTask
 {
     public partial class Page3 : Page
     {
+        #region Исходные данные
+        private int[]? _sourceArray;
+        private int[]? _newArray;
+        #endregion
+
         public Page3()
         {
             InitializeComponent();
+            InitializeArray(); 
         }
+
+        #region Инициализация массива
+        private void InitializeArray()
+        {
+            Random random = new Random();
+            _sourceArray = Enumerable.Range(0, 8).Select(g => random.Next(10, 100)).ToArray();
+            for (int i = 0; i < _sourceArray.Length; i++)
+            {
+                TbSourceArray.Text += $" {_sourceArray[i]}";
+            }
+        }
+        #endregion
+
+        #region Обработка нажатия кнопки "Получить новый массив"
+        private void BtnGetNewArray_Click(object sender, RoutedEventArgs e)
+        {
+            SpNewArray.Visibility = Visibility.Visible; 
+            TbNewArray.Text = ""; 
+
+            _newArray = _sourceArray?.Select(g => g % 10).ToArray(); 
+            for (int i = 0; i < _newArray?.Length; i++)
+            {
+                TbNewArray.Text += $" {_newArray[i]}"; 
+            }
+        }
+        #endregion
     }
 }
