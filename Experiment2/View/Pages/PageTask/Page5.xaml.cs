a4b6b868fb4afd3e358fc6d68afcae4b07e17648
1hp <keshasuper396@gmail.com>
2025-03-19 18:20:03 +0300

    Page5

--- Experiment2/View/Pages/PageTask/Page5.xaml.cs
+++ Experiment2/View/Pages/PageTask/Page5.xaml.cs
@@ -13,16 +13,64 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Experiment2.View.Pages.PageTask
+using System;
+
+namespace CourseWorkApp.View.Pages.PageTask
 {
-    /// <summary>
-    /// Логика взаимодействия для Page5.xaml
-    /// </summary>
     public partial class Page5 : Page
     {
+        #region Исходные данные
+        private int n = 5; 
+        private int[,]? _sourceArray; 
+        #endregion
+
         public Page5()
         {
             InitializeComponent();
+            InitializeArray(); 
         }
+
+        #region Инициализация массива
+        private void InitializeArray()
+        {
+            _sourceArray = new int[n, n];
+            Random random = new Random();
+            TbSourceArray.Text = ""; 
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    _sourceArray[i, j] = random.Next(10, 100);
+                    TbSourceArray.Text += $"{_sourceArray[i, j]}\t"; 
+                }
+                TbSourceArray.Text += "\n";
+            }
+        }
+        #endregion
+
+        #region Обработка нажатия кнопки "Модифицировать массив"
+        private void BtnModified_Click(object sender, RoutedEventArgs e)
+        {
+            SpModifiedArray.Visibility = Visibility.Visible;
+            TbModifiedArray.Text = ""; 
+
+           
+            for (int i = 0; i < n; i++)
+            {
+                (_sourceArray[3, i], _sourceArray[4, i]) = (_sourceArray[4, i], _sourceArray[3, i]);
+            }
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    TbModifiedArray.Text += $"{_sourceArray[i, j]}\t"; 
+                }
+                TbModifiedArray.Text += "\n";
+            }
+        }
+        #endregion
     }
 }
